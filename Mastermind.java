a23dd0ddb79af4a5c334d5eb54c10b64ebb24117
airkahlynn <brownwinte1@nku.edu>
2015-04-29 23:46:07 -0400

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -3,16 +3,16 @@ import java.text.*;
 
 public class Mastermind {  
 	
-	//Main method
-    public void main(String[] args) 
+    public static void main(String[] args) 
     {
        Board board = new Board();
        String code = generateCode();
        boolean gameover = false;
        Scanner input = new Scanner(System.in);
+       String guess = board.getPlayerGuess(input);
        
        do{
-       	String guess = board.getPlayerGuess(input);
+       	guess = board.getPlayerGuess(input);
        	if (!(guess.equals("QUIT"))){
        		gameover = board.processGuess(guess, code );
        		board.printBoard();
@@ -26,19 +26,21 @@ public class Mastermind {
        	else
        		System.out.println("Sorry - too many guesses");
     }
-    
-    //Generates secret code
-	public String generateCode()
+   
+	public static String generateCode()
 	{
 		Random gen = new Random();
 		int num = 0;
 		char[] secretCode = new char[4];
+		String secretString = "" + secretCode;
 		char[] colors = {'R','B','G','Y'};
 		for(int x = 0; x < colors.length; x++)
 		{ 
 			num = gen.nextInt(4);
 			secretCode[x] = colors[num];
+			System.out.println(secretString);
 		}
-		return secretCode;
+		return secretString;
       }
+     
 }//End
